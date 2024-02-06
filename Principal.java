5c7e5d6a0e903dc80bd2b98e08dd4d401c0c0c83
unknown <4106331@alu.murciaeduca.es>
2024-02-06 02:33:50 +0100

    Modificado Principal.java (mostrar fecha y hora actual) y README.md

--- Principal.java
+++ Principal.java
@@ -1,5 +1,20 @@
+import java.time.*;
+
 public class Principal {
-	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        fechaHoraActual();
+    }
+
+    public static void fechaHoraActual() {
+		String reloj = "";
+		String dia = ((LocalDate.now().getDayOfMonth() < 10) ? "0" : "") + LocalDate.now().getDayOfMonth();
+		String mes = ((LocalDate.now().getMonthValue() < 10) ? "0" : "") + LocalDate.now().getMonthValue();
+		String anio = "" + LocalDate.now().getYear();
+		String horas = ((LocalTime.now().getHour() < 10) ? "0" : "") + LocalTime.now().getHour();
+		String minutos = ((LocalTime.now().getMinute() < 10) ? "0" : "") + LocalTime.now().getMinute();
+		String segundos = ((LocalTime.now().getSecond() < 10) ? "0" : "") + LocalTime.now().getSecond();
+		reloj = dia + "/" + mes + "/" + anio + " " + horas + ":" + minutos + ":" + segundos;
+        System.out.println(reloj);
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# EntornosPractica4_1
-Este es un repositorio para la práctica 4_1 de Entornos de Desarrollo
+## [MAIN](https://github.com/DanielLopezOlmos7/EntornosPractica4_1/tree/main) Programa principal
+*<b>[Rama Main](https://github.com/DanielLopezOlmos7/EntornosPractica4_1/tree/main)</b> :point_right: Esta es la rama principal de esta práctica.*
+## [PRACTICA4_1](https://github.com/DanielLopezOlmos7/EntornosPractica4_1/tree/Practica4_1) Hora Actual :clock10:
+*<b>[Rama Practica4_1](https://github.com/DanielLopezOlmos7/EntornosPractica4_1/tree/Practica4_1)</b> :point_right: Esta rama se encarga de modificar Principal.java para mostrar por pantalla la hora actual.*
